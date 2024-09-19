f99be72de9e253dcf9d5c18cbb1ae263f6615cd1
laynechensquare <laynechensquare@gmail.com>
2024-09-19 08:51:04 +0800

    feat: adds border to parallax shaders

--- shaders/animated-parallax-depth/fragment.js
+++ shaders/animated-parallax-depth/fragment.js
@@ -1,35 +1,67 @@
 const fragmentShader = `
 varying vec3 position;
 varying vec2 vUv;
+uniform vec2 uResolution;
+uniform vec4 uRadii;
+
 uniform sampler2D uTexture;
 uniform sampler2D uTextureDepth;
 uniform vec2 uMouse;
 
+float roundedBoxSDF(vec2 centerPosition, vec2 size, vec4 radius) {
+    radius.xy = (centerPosition.x > 0.0) ? radius.xy : radius.zw;
+    radius.x = (centerPosition.y > 0.0) ? radius.x : radius.y;
+    vec2 q = abs(centerPosition) - size + radius.x;
+    return min(max(q.x, q.y), 0.0) + length(max(q, 0.0)) - radius.x;
+}
+
 void main() {
-  float frequency = 100.0;
-  float amplitude = 0.003;
-  float distortion = sin(vUv.y * frequency) * amplitude;
+    // ! parallax handling
+    float frequency = 100.0;
+    float amplitude = 0.003;
+    float distortion = sin(vUv.y * frequency) * amplitude;
 
-  // - output the original with distortion
-  // vec4 color = texture(uTexture, vec2(vUv.x + distortion, vUv.y));
+    // - sample the depth texture
+    float depth = texture(uTextureDepth, vUv).r;
+    float shiftX = uMouse.x * 0.05;
+    float shiftY = uMouse.y * 0.05;
 
-  // - output the original
-  // vec4 color = texture(uTexture, vUv);
+    vec2 parallaxPosition= vec2(vUv.x + shiftX * depth, vUv.y + shiftY * depth);
+    vec4 textureColorWithParallax = texture(uTexture, parallaxPosition);
 
-  // - sample the depth texture
-  float depth = texture(uTextureDepth, vUv).r;
+    // ! border handling
+    vec2 pixelPosition = vUv * uResolution;
+    vec2 centerPosition = pixelPosition - uResolution * 0.5;
+    float borderWidth = 2.0;
+    vec2 size = uResolution * 0.5 - borderWidth;
+    float distance = roundedBoxSDF(centerPosition, size, uRadii);
 
-  // float parallax = uMouse * 0.005;
+    // - Smooth alpha for the border and the fill
+    float smoothedAlpha = 1.0 - smoothstep(0.0, 1.0, distance);
 
-  float shiftX = uMouse.x * 0.01;
-  float shiftY = uMouse.y * 0.01;
+    // - Colors
+    vec3 borderColor = vec3(1.0, 1.0, 0.941); // #fffff0 in RGB
+    vec3 fillColor = vec3(textureColorWithParallax.r, textureColorWithParallax.g, textureColorWithParallax.b);
 
-  vec2 parallaxPosition= vec2(vUv.x + shiftX * depth, vUv.y + shiftY * depth);
+    // - Determine the alpha for fill and border
+    float fillAlpha = 1.0;        // Make the fill fully transparent
+    float borderAlpha = 1.0;      // Keep the border fully opaque
 
-  vec4 color = texture(uTexture, parallaxPosition);
+    // - Mix the fill and border colors based on the distance
+    vec3 color = mix(fillColor, borderColor, smoothstep(-borderWidth, 0.0, distance));
+    float alpha = mix(fillAlpha, borderAlpha, smoothstep(-borderWidth, 0.0, distance));
 
-  gl_FragColor = color;
+    csm_FragColor = vec4(color, alpha * smoothedAlpha);
 }
 `;
 
 export default fragmentShader;
+
+// - output the original with distortion
+// vec4 color = texture(uTexture, vec2(vUv.x + distortion, vUv.y));
+
+// - output the original
+// vec4 color = texture(uTexture, vUv);
+
+// vec4 color = texture(uTexture, parallaxPosition);
+// csm_FragColor = color;
--- shaders/animated-parallax-depth/vertex.js
+++ shaders/animated-parallax-depth/vertex.js
@@ -6,6 +6,6 @@ void main() {
     vUv = uv;
     vPosition = position;
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    csm_PositionRaw = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
 `;
