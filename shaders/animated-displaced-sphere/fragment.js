6d0ece65a5ce8bac38f056c516a773858f5627f9
laynechensquare <laynechensquare@gmail.com>
2024-10-02 22:14:39 +0800

    feat: adjust ball frag shader logic for selective color shading

--- shaders/animated-displaced-sphere/fragment.js
+++ shaders/animated-displaced-sphere/fragment.js
@@ -1,11 +1,17 @@
 export default /*glsl*/ `
 varying float vPattern;
-
 uniform vec3 uColor;
+uniform float uIsNormalColor;
 
 void main() {
-    vec3 color = vPattern * uColor;
+    vec3 color = uColor; // Default to uniform color
 
-    csm_DiffuseColor = vec4(color, 1);
+    if (uIsNormalColor == 1.0) {
+        vec3 nv_color = normalize(vNormal) * 0.5 + 0.5; // Normal vector color
+        color = nv_color; // Use normal color when the condition is true
+    }
+
+    color *= vPattern;
+    csm_DiffuseColor = vec4(color, 1.0);
 }
 `;
