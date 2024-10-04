5fc2b79a934c5f65d5fd6671aa925e2da17237e7
laynechensquare <laynechensquare@gmail.com>
2024-10-04 16:06:35 +0800

    feat: add jitter effect to rounded rec vertex shader

--- shaders/rounded-rectangle/vertex.js
+++ shaders/rounded-rectangle/vertex.js
@@ -5,5 +5,9 @@ varying vec2 vUv;
 void main() {
     vUv = uv;
     csm_PositionRaw = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+    // - jitter effect
+    // csm_PositionRaw.xy /= csm_PositionRaw.w;
+    // csm_PositionRaw.xy = floor(csm_PositionRaw.xy * 80.0) / 80.0 * csm_PositionRaw.w;
 }
 `;
