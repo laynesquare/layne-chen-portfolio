3a488ae11fa4fd037bb7459edc34b74b490eba99
laynechensquare <laynechensquare@gmail.com>
2024-10-04 16:10:53 +0800

    refactor: displaced sphere vertex comments

--- shaders/animated-displaced-sphere/vertex.js
+++ shaders/animated-displaced-sphere/vertex.js
@@ -9,9 +9,7 @@ uniform float uNoiseStrength;
 uniform float uDisplacementStrength;
 uniform float uFractAmount;
 
-//	Classic Perlin 3D Noise 
-//	by Stefan Gustavson (https://github.com/stegu/webgl-noise)
-//
+// - Classic Perlin 3D Noise 
 vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
 vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
 vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
@@ -109,6 +107,7 @@ void main() {
     vPattern = pattern;
 
     csm_Position += csm_Normal * pattern;
+
     posA += csm_Normal * getDisplacement(posA);
     posB += csm_Normal * getDisplacement(posB);
 
