48825ab15195121a3e51e12839ce7de66eef3ad6
laynechensquare <laynechensquare@gmail.com>
2024-10-11 17:34:01 +0800

    feat: main scene component custom progress monitor hook for perf (avoid constant state changes and re-rendering)

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,21 @@ import { ScrollScene } from '@14islands/r3f-scroll-rig';
 
 import { useDomStore } from '@/store';
 
+import { useWebGlStore } from '@/store';
+
 // const Scene = dynamic(() => import('@/components/scenery/Scene'), { ssr: false });
+function ProgressMonitor() {
+	const progress = useProgress(state => state.progress);
+	const loadedRegister = useWebGlStore(state => state.loadedRegister);
+
+	useEffect(() => {
+		if (progress === 100) {
+			loadedRegister(true);
+		}
+	}, [progress]);
+
+	return null;
+}
 
 export default function Home() {
 	const wrapperRef = useRef(null);
@@ -23,19 +37,8 @@ export default function Home() {
 
 	return (
 		<>
-			<Loader />
-			{/* <main
-				className={`overall-ctn`}
-				ref={wrapperRef}
-				style={{
-					transformOrigin: 'center',
-					transformStyle: 'preserve-3d',
-					transform: 'rotateX(10deg)',
-					backgroundColor: 'red',
-				}}>
-				<Hero contentRef={contentRef} />
-				<Scene wrapperRef={wrapperRef} />
-			</main> */}
+			{/* <Loader /> */}
+			<ProgressMonitor />
 
 			<main ref={wrapperRef}>
 				<Hero />
