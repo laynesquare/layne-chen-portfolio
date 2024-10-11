fbf31109ae597bee305f9f7daff3d512a5a1b903
laynechensquare <laynechensquare@gmail.com>
2024-10-11 17:29:27 +0800

    feat: intro animation for the whole canvas

--- components/scenery/Scene.tsx
+++ components/scenery/Scene.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Suspense, useEffect, useRef } from 'react';
+import { Suspense, use, useEffect, useRef } from 'react';
 import { Canvas, useThree, useFrame } from '@react-three/fiber';
 import {
 	View,
@@ -45,19 +45,20 @@ import { suspend } from 'suspend-react';
 
 import { ReactLenis, useLenis } from '@studio-freight/react-lenis';
 import dynamic from 'next/dynamic';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { useWebGlStore } from '@/store';
 
 const Port = dynamic(() => import('@/components/scenery/Port'), { ssr: false });
 
-export default function Scene({ wrapperRef }: SceneProps) {
-	const scroll = useScroll();
+gsap.registerPlugin(useGSAP);
 
-	// Example of scrolling to 50% of the scroll height
-	// useEffect(() => {
-	// 	scroll.el.scrollTop = 100;
-	// }, []);
+export default function Scene({ wrapperRef }: SceneProps) {
+	const canvasRef = useRef(null);
 
 	return (
 		<Canvas
+			ref={canvasRef}
 			gl={{
 				antialias: true,
 				alpha: false,
@@ -72,13 +73,47 @@ export default function Scene({ wrapperRef }: SceneProps) {
 				left: 0,
 				width: '100lvw',
 				height: '100lvh',
+				pointerEvents: 'all',
 			}}
 			dpr={[1.1, 1.1]}
 			camera={{ position: [0, 0, 8], fov: 30 }}
 			resize={{ debounce: 100 }}
 			flat={true}
 			eventSource={wrapperRef?.current}>
+			<Disclose canvasRef={canvasRef} />
 			<Port />
+			<Preload all />
 		</Canvas>
 	);
+}
+
+function Disclose({ canvasRef }) {
+	const isLoaded = useWebGlStore(state => state.isLoaded);
+
+	useGSAP(
+		() => {
+			if (isLoaded) {
+				gsap.to(canvasRef.current, {
+					// delay: 0,
+					duration: 5,
+					maskSize: '100rem',
+					ease: 'power2.inOut',
+				});
+			} else {
+				gsap.to(canvasRef.current, {
+					duration: 0,
+					WebkitMaskImage: "url('/frame/star.c9387850.svg')",
+					maskImage: "url('/frame/star.c9387850.svg')",
+					WebkitMaskRepeat: 'no-repeat',
+					maskRepeat: 'no-repeat',
+					WebkitMaskPosition: 'center center',
+					maskPosition: 'center center',
+					maskSize: '50rem',
+				});
+			}
+		},
+		{ dependencies: [isLoaded], scope: canvasRef },
+	);
+
+	return null;
 }
