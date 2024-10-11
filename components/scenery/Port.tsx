af1341fee6776a8c389c67c214e94edb76ee3203
laynechensquare <laynechensquare@gmail.com>
2024-10-11 17:30:23 +0800

    feat: use Precompile for perf (effects not substantial)

--- components/scenery/Port.tsx
+++ components/scenery/Port.tsx
@@ -1,34 +1,7 @@
-import { Suspense, useEffect, useRef } from 'react';
+import { Suspense, useEffect, useLayoutEffect, useRef } from 'react';
 import { Canvas, useThree, useFrame } from '@react-three/fiber';
-import {
-	View,
-	useProgress,
-	Html,
-	ScrollControls,
-	useScroll,
-	OrbitControls,
-	Text,
-	Preload,
-	MeshTransmissionMaterial,
-	Text3D,
-	Environment,
-	Lightformer,
-	Scroll,
-	Sparkles,
-} from '@react-three/drei';
-import {
-	EffectComposer,
-	Bloom,
-	ChromaticAberration,
-	Noise,
-	ToneMapping,
-	Scanline,
-	GodRays,
-	LensFlare,
-	N8AO,
-	FXAA,
-} from '@react-three/postprocessing';
-import { KernelSize, Resolution, BlendFunction } from 'postprocessing';
+import { Preload, Environment, Lightformer } from '@react-three/drei';
+
 import { Vector2, BackSide, Vector3, Color } from 'three';
 
 import Ripple from '@/components/scenery/Ripple';
@@ -40,18 +13,18 @@ export default function Port() {
 	return (
 		<>
 			{/* <OrbitControls /> */}
-			<Suspense>
-				{/* <ScrollControls
-					enabled={false}
-					damping={0.1}
-					pages={}> */}
+			<Suspense fallback={null}>
+				<Precompile />
 				<Ripple>
 					<Banner />
 					<Model />
-					{/* <Camera /> */}
-					<Environment preset='warehouse'>
+					<Environment
+						// files='/scenery/textures/empty_warehouse.jpg'
+						resolution={64}
+						preset='warehouse'>
 						<Lightformer
-							color='white'
+							color='#FFFFF0'
+							// intensity={10}
 							intensity={3}
 							position={[10, 5, 0]}
 							scale={[10, 50, 1]}
@@ -59,10 +32,14 @@ export default function Port() {
 							form='rect'
 						/>
 					</Environment>
-					<Preload all />
 				</Ripple>
-				{/* </ScrollControls> */}
 			</Suspense>
 		</>
 	);
+}
+
+function Precompile() {
+	const { gl, scene, camera } = useThree();
+	useLayoutEffect(() => void gl.compile(scene, camera), []);
+	return null;
 }
