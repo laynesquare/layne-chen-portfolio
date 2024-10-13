fbc5a67dfad13bfea234b07d45798e29b5482517
laynechensquare <laynechensquare@gmail.com>
2024-10-13 18:58:49 +0800

    feat: intro loading page animation and notification upon animation done

--- components/scenery/Loader.tsx
+++ components/scenery/Loader.tsx
@@ -8,116 +8,190 @@ import { useWebGlStore } from '@/store';
 gsap.registerPlugin(useGSAP);
 
 export default function Loader() {
-	const progress = useProgress(state => state.progress);
-	const item = useProgress(state => state.item);
+	const progress = useProgress();
 	const isLoaded = useWebGlStore(state => state.isLoaded);
 	const loaderRef = useRef(null);
 
 	useGSAP(
 		() => {
 			if (isLoaded) {
-				gsap.to(loaderRef.current, {
-					delay: 0,
-					opacity: 0,
-					duration: 1,
-					ease: 'power2.inOut',
-				});
+				const tl = gsap.timeline();
+				tl.to(loaderRef.current, {
+					rotate: 180,
+					scale: 10,
+					duration: 0.6,
+					ease: 'none',
+				}).to(
+					loaderRef.current,
+					{
+						pointerEvents: 'none',
+						opacity: 0,
+						duration: 0.3,
+						ease: 'none',
+						onComplete: () => {
+							useWebGlStore.setState({ isEntryAnimationDone: true });
+						},
+					},
+					'<',
+				);
 			}
 		},
-		{ dependencies: [isLoaded], scope: loaderRef },
-	);
-
-	useGSAP(
-		() => {
-			const tl = gsap.timeline();
-
-			// if (isOpen) {
-			// 	gsap.to(overlayNavRef.current, { opacity: 1, pointerEvents: 'auto', duration: 0.2, ease: 'sine.in' });
-			// 	tl.to('[data-row="action"]', { x: 0, duration: 1, ease: 'elastic.out(0.75, 0.5)' })
-			// 		.to('[data-row="home"]', { x: 0, duration: 1, ease: 'elastic.out(0.75, 0.5)' }, '<5%')
-			// 		.to('[data-row="about"]', { x: 0, duration: 1, ease: 'elastic.out(0.75, 0.5)' }, '<5%')
-			// 		.to('[data-row="skill"]', { x: 0, duration: 1, ease: 'elastic.out(0.75, 0.5)' }, '<5%')
-			// 		.to('[data-row="experience"]', { x: 0, duration: 1, ease: 'elastic.out(0.75, 0.5)' }, '<5%')
-			// 		.to('[data-row="project"]', { x: 0, duration: 1, ease: 'elastic.out(0.75, 0.5)' }, '<5%')
-			// 		.to('[data-row="contact"]', { x: 0, duration: 1, ease: 'elastic.out(0.75, 0.5)' }, '<5%');
-			// }
-
-			// if (progress === 100) {
-			// 	gsap.to(loaderRef.current, {
-			// 		delay: 2,
-			// 		opacity: 0,
-			// 		duration: 0.2,
-			// 		ease: 'power2.inOut',
-			// 	});
-			// }
-		},
-		// { dependencies: [progress], scope: loaderRef },
+		{ dependencies: [isLoaded] },
 	);
 
 	return (
 		<>
 			<div
 				ref={loaderRef}
-				className={`z-[100] fixed top-0 left-0 w-lvw h-lvh bg-primary text-neutral font-boxing leading-none pointer-events-none`}>
-				<Indicator
-					progress={progress}
-					item={item}
-				/>
+				className={`z-[100] fixed top-0 left-0 w-lvw h-lvh bg-primary text-neutral font-boxing leading-none`}>
+				<div
+					data-intro-container
+					className='h-full w-full whitespace-pre-line relative'
+					style={{
+						transform: `translate(-6rem, -6.5rem)`,
+					}}>
+					<p
+						data-intro-item
+						className='text-2xl text-right absolute flex justify-center items-center h-full w-full'
+						style={{
+							transform: `rotateX(-45deg) rotateY(-45deg) rotateZ(0deg) translate3d(9.375rem, -6.25rem, -8.125rem)`,
+						}}>
+						{truncate(progress.item, 40)}
+					</p>
+					<p
+						className='text-right text-[11.75rem] absolute flex justify-center items-center h-full w-full'
+						data-intro-name
+						style={{
+							transform: `rotateX(45deg) rotateY(0deg) rotateZ(45deg)`,
+						}}>
+						<span>{`LAYNE\nCHEN`}</span>
+					</p>
+					<p
+						data-intro-loading
+						className='text-right text-[9.875rem] absolute flex justify-center items-center h-full w-full'
+						style={{
+							transform: `rotateX(-45deg) rotateY(-45deg) rotateZ(0deg) translate3d(-12.5rem, 37.5rem, -8.125rem)`,
+							color: `var(--color-bg-primary)`,
+							WebkitTextStroke: `0.25rem var(--color-font-neutral)`,
+						}}>
+						<span>{`LOAD\nING`}</span>
+					</p>
+					<p
+						data-intro-progress
+						className='text-right text-[9.875rem] absolute flex justify-center items-center h-full w-full text-neutralContrast'
+						style={{
+							transform: `translate3d(23rem, 18rem, 0rem) rotateX(45deg) rotateY(-45deg) rotateZ(0deg) skew(0,0) scale(1, 2.25)`,
+							// scale: `1 2.25`,
+							opacity: 0.5,
+						}}>
+						<span>{`${progress.progress.toFixed(0).padStart(3, '0')}%`}</span>
+					</p>
+				</div>
 			</div>
 		</>
 	);
 }
 
+// {
+// 	<>
+// 		<div
+// 			ref={loaderRef}
+// 			className={`z-[100] fixed top-0 left-0 w-lvw h-lvh bg-primary text-neutral font-boxing leading-none`}>
+// 			<div
+// 				className='h-full w-full whitespace-pre-line relative'
+// 				style={{
+// 					transform: `translate(-6rem, -6.5rem)`,
+// 				}}>
+// 				<p
+// 					data-intro-item
+// 					className='text-2xl text-right absolute flex justify-center items-center h-full w-full'
+// 					style={{
+// 						transform: `rotateX(-45deg) rotateY(-45deg) rotateZ(0deg) translate3d(9.375rem, -6.25rem, -8.125rem)`,
+// 					}}>
+// 					{truncate(progress.item, 40)}
+// 				</p>
+// 				<p
+// 					className='text-right text-[11.75rem] absolute flex justify-center items-center h-full w-full'
+// 					data-intro-name
+// 					style={{
+// 						transform: `rotateX(45deg) rotateY(0deg) rotateZ(45deg)`,
+// 					}}>
+// 					<span>{`LAYNE\nCHEN`}</span>
+// 				</p>
+// 				<p
+// 					data-intro-loading
+// 					className='text-right text-[9.875rem] absolute flex justify-center items-center h-full w-full'
+// 					style={{
+// 						transform: `rotateX(-45deg) rotateY(-45deg) rotateZ(0deg) translate3d(-12.5rem, 37.5rem, -8.125rem)`,
+// 						color: `var(--color-bg-primary)`,
+// 						WebkitTextStroke: `0.25rem var(--color-font-neutral)`,
+// 					}}>
+// 					<span>{`LOAD\nING`}</span>
+// 				</p>
+// 				<p
+// 					data-intro-progress
+// 					className='text-right text-[9.875rem] absolute flex justify-center items-center h-full w-full text-neutralContrast'
+// 					style={{
+// 						transform: `rotateX(45deg) rotateY(-45deg) rotateZ(0deg) translate3d(25rem, 38rem, -8.125rem) scaleY(2.25)`,
+// 						opacity: 0.5,
+// 					}}>
+// 					<span>{`${progress.progress.toFixed(0).padStart(3, '0')}%`}</span>
+// 				</p>
+// 			</div>
+// 		</div>
+// 	</>;
+// }
+
 function truncate(str, maxLength) {
 	return str.length > maxLength ? `${str.substring(0, maxLength)}...` : str;
 }
 
-function Indicator({ progress, item }) {
-	const [isClient, setIsClient] = useState(false);
-
-	useEffect(() => {
-		setIsClient(true);
-	}, []);
-
-	if (!isClient) return null;
-
-	return (
-		<div
-			className='h-full w-full whitespace-pre-line relative'
-			style={{
-				transform: `translate(-6rem, -6.5rem)`,
-			}}>
-			<p
-				className='text-2xl text-right absolute flex justify-center items-center h-full w-full'
-				style={{
-					transform: `rotateX(-45deg) rotateY(-45deg) rotateZ(0deg) translate3d(9.375rem, -6.25rem, -8.125rem)`,
-				}}>
-				{truncate(item, 40)}
-			</p>
-			<p
-				className='text-right text-[11.75rem] absolute flex justify-center items-center h-full w-full'
-				style={{
-					transform: `rotateX(45deg) rotateY(0deg) rotateZ(45deg)`,
-				}}>
-				<span>{`LAYNE\nCHEN`}</span>
-			</p>
-			<p
-				className='text-right text-[9.875rem] absolute flex justify-center items-center h-full w-full'
-				style={{
-					transform: `rotateX(-45deg) rotateY(-45deg) rotateZ(0deg) translate3d(-12.5rem, 37.5rem, -8.125rem)`,
-					color: `var(--color-bg-primary)`,
-					WebkitTextStroke: `0.25rem var(--color-font-neutral)`,
-				}}>
-				<span>{`LOAD\nING`}</span>
-			</p>
-			<p
-				className='text-right text-[9.875rem] absolute flex justify-center items-center h-full w-full text-neutralContrast'
-				style={{
-					transform: `rotateX(45deg) rotateY(-45deg) rotateZ(0deg) translate3d(25rem, 38rem, -8.125rem) scaleY(2.25)`,
-					opacity: 0.5,
-				}}>
-				<span>{`${progress.toFixed(0).padStart(3, '0')}%`}</span>
-			</p>
-		</div>
-	);
+{
+	// 	<>
+	// 		<div
+	// 			ref={loaderRef}
+	// 			className={`z-[100] fixed top-0 left-0 w-lvw h-lvh bg-primary text-neutral font-boxing leading-none`}>
+	// 			<div
+	// 				className='h-full w-full whitespace-pre-line relative'
+	// 				style={{
+	// 					transform: `translate(-6rem, -6.5rem)`,
+	// 				}}>
+	// 				<p
+	// 					data-intro-item
+	// 					className='text-2xl text-right absolute flex justify-center items-center h-full w-full'
+	// 					style={{
+	// 						transform: `rotateX(-45deg) rotateY(-45deg) rotateZ(0deg) translate3d(9.375rem, -6.25rem, -8.125rem)`,
+	// 					}}>
+	// 					{truncate(progress.item, 40)}
+	// 				</p>
+	// 				<p
+	// 					className='text-right text-[11.75rem] absolute flex justify-center items-center h-full w-full'
+	// 					data-intro-name
+	// 					style={{
+	// 						transform: `rotateX(45deg) rotateY(0deg) rotateZ(45deg)`,
+	// 					}}>
+	// 					<span>{`LAYNE\nCHEN`}</span>
+	// 				</p>
+	// 				<p
+	// 					data-intro-loading
+	// 					className='text-right text-[9.875rem] absolute flex justify-center items-center h-full w-full'
+	// 					style={{
+	// 						transform: `rotateX(-45deg) rotateY(-45deg) rotateZ(0deg) translate3d(-12.5rem, 37.5rem, -8.125rem)`,
+	// 						color: `var(--color-bg-primary)`,
+	// 						WebkitTextStroke: `0.25rem var(--color-font-neutral)`,
+	// 					}}>
+	// 					<span>{`LOAD\nING`}</span>
+	// 				</p>
+	// 				<p
+	// 					data-intro-progress
+	// 					className='text-right text-[9.875rem] absolute flex justify-center items-center h-full w-full text-neutralContrast'
+	// 					style={{
+	// 						transform: `rotateX(45deg) rotateY(-45deg) rotateZ(0deg) translate3d(25rem, 38rem, -8.125rem) scaleY(2.25)`,
+	// 						opacity: 0.5,
+	// 					}}>
+	// 					<span>{`${progress.progress.toFixed(0).padStart(3, '0')}%`}</span>
+	// 				</p>
+	// 			</div>
+	// 		</div>
+	// 	</>;
 }
