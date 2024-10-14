bd351411358eaf81fa0676aa08db846b33326cc2
laynechensquare <laynechensquare@gmail.com>
2024-10-14 23:33:02 +0800

    refactor: rm unnecessary font import

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,6 @@ import localFont from 'next/font/local';
 
 import './globals.css';
 
-// const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
-// const kronaOne = Krona_One({ weight: '400', subsets: ['latin'], variable: '--font-krona-one' });
-// const clashDisplay = localFont({ src: '../public/font/ClashDisplay.ttf', variable: '--font-clash-display' });
 const boxing = localFont({ src: '../public/font/Boxing-Regular.woff', variable: '--font-boxing' });
 const satoshi = localFont({ src: '../public/font/Satoshi-Bold.woff', variable: '--font-satoshi' });
 
