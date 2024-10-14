979f21ac627ca7b561b02ca1a0a8b95b0611aa16
laynechensquare <laynechensquare@gmail.com>
2024-10-14 23:33:49 +0800

    refactor: rm used code of dynamic importing

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 'use client';
-import { useEffect, useRef, useState } from 'react';
-import dynamic from 'next/dynamic';
+import { useEffect, useRef } from 'react';
+
 import Hero from '@/components/frame/Hero';
 import Scene from '@/components/scenery/Scene';
 import Loader from '@/components/scenery/Loader';
 
-// const Scene = dynamic(() => import('@/components/scenery/Scene'), { ssr: false });
-
 export default function Home() {
 	const wrapperRef = useRef(null);
 
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
+	useEffect(() => window.scrollTo(0, 0), []);
 
 	return (
 		<>
