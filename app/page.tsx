a82ecbe57f482d43bd52df6544244e6f031d1943
laynechensquare <laynechensquare@gmail.com>
2024-10-18 17:46:18 +0800

    feat: usePlatformMonitor hook

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Hero from '@/components/frame/Hero';
 import Scene from '@/components/scenery/Scene';
 import Loader from '@/components/scenery/Loader';
 
+import { usePlatformStore } from '@/store';
+
 export default function Home() {
 	const wrapperRef = useRef(null);
 
@@ -13,10 +15,26 @@ export default function Home() {
 	return (
 		<>
 			<Loader />
+			<PlatformMonitor />
 			<main ref={wrapperRef}>
 				<Hero />
 				<Scene wrapperRef={wrapperRef} />
 			</main>
 		</>
 	);
+}
+
+function PlatformMonitor() {
+	function updateIsMobile() {
+		const isMobile = window.innerWidth < 768;
+		usePlatformStore.setState({ isMobile });
+	}
+
+	useEffect(() => {
+		updateIsMobile();
+		window.addEventListener('resize', updateIsMobile);
+		return () => window.removeEventListener('resize', updateIsMobile);
+	}, []);
+
+	return null;
 }
