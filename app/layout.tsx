4bc9b6d7f376362dc5731ccd0fc3d1a874c1c6c6
laynechensquare <laynechensquare@gmail.com>
2024-10-28 23:13:50 +0800

    feat: keyword meta tags in html

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Head from 'next/head';
 import SmoothScrolling from '@/components/SmoothScroll';
 import localFont from 'next/font/local';
 
@@ -10,8 +11,41 @@ const fonts = `${boxing.variable} ${satoshi.variable}`;
 
 export const metadata: Metadata = {
 	title: 'Layne Chen | Front-end Developer',
+	creator: 'Layne Chen',
+	generator: 'Next.js',
+	referrer: 'origin-when-cross-origin',
 	description:
 		'Proficient in TypeScript/JavaScript, React, Angular, Node.js, and MongoDB. Skilled in SSG/SSR, RESTful APIs, and UX design, with a focus on performance and aesthetics.',
+	keywords: [
+		'Frontend',
+		'Backend',
+		'TypeScript (JavaScript)',
+		'HTML',
+		'CSS/Sass',
+		'Next.js',
+		'React Router',
+		'React Redux',
+		'Angular',
+		'Angular Universal',
+		'RxJS',
+		'Karma/Jasmine',
+		'Tailwind CSS',
+		'Bootstrap',
+		'WebGL',
+		'Three.js',
+		'GSAP',
+		'Node.js',
+		'Express',
+		'Mongoose',
+		'MongoDB',
+		'RESTful API',
+		'Git (Sourcetree)',
+		'GitLab',
+		'Webpack',
+		'Figma',
+		'Illustrator',
+		'Photoshop',
+	],
 };
 
 export default function RootLayout({
