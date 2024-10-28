be3e32ee30fee4f2e9d135983d8dcb1f8f1b060d
laynechensquare <laynechensquare@gmail.com>
2024-10-28 23:11:24 +0800

    feat: stopPropagate and gsap anim for perf

--- components/scenery/Model.tsx
+++ components/scenery/Model.tsx
@@ -43,6 +43,7 @@ import {
 	Euler,
 	MeshBasicMaterial,
 	CircleGeometry,
+	NoBlending,
 } from 'three';
 
 import { ReactLenis, useLenis } from '@studio-freight/react-lenis';
@@ -55,7 +56,7 @@ import { mergeVertices } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import { useControls } from 'leva';
 import { lerp } from 'three/src/math/MathUtils.js';
 
-import { useDomStore, usePlatformStore } from '@/store';
+import { useDomStore, usePlatformStore, useWebGlStore } from '@/store';
 
 // gsap
 import gsap from 'gsap';
@@ -65,13 +66,6 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export default memo(function Model() {
-	// const viewport = useThree(state => state.viewport);
-	// const size = useThree(state => state.size);
-	// const camera = useThree(state => state.camera);
-	// const scene = useThree(state => state.scene);
-	// const [viewport] = useThree(state => [state.viewport]);
-	// const anchorDomEls = useDomStore(state => state.anchorEls);
-
 	const getThree = useThree(state => state.get);
 
 	const ballRef = useRef();
@@ -81,11 +75,13 @@ export default memo(function Model() {
 	const ballDynamicPosRef = useRef(new Vector3());
 	const ballClonedDynamicPosRef = useRef(new Vector3());
 	const ballMeshRatio = 1 - getThree().viewport.factor / calcFactorCamZ(1);
-	const ballGeometry = useMemo(() => {
-		const geometry = mergeVertices(new IcosahedronGeometry(0.5, 64));
-		geometry.computeTangents();
-		return geometry;
-	}, []);
+	const ballGeometry = useRef(
+		(() => {
+			const geometry = mergeVertices(new IcosahedronGeometry(0.5, 64));
+			geometry.computeTangents();
+			return geometry;
+		})(),
+	);
 
 	const ballMaskRef = useRef(null);
 	const ballClonedMaskRef = useRef(null);
@@ -94,68 +90,9 @@ export default memo(function Model() {
 
 	const displacementTexture = useLoader(TextureLoader, '/scenery/textures/test2.jpg');
 
-	// console.log('re render from ball');
-
-	// const {
-	// 	gradientStrength,
-	// 	color,
-	// 	speed,
-	// 	noiseStrength,
-	// 	displacementStrength,
-	// 	fractAmount,
-	// 	roughness,
-	// 	metalness,
-	// 	clearcoat,
-	// 	reflectivity,
-	// 	ior,
-	// 	iridescence,
-	// 	iridescenceIOR,
-	// 	transmission,
-	// 	thickness,
-	// 	dispersion,
-	// } = useControls({
-	// 	gradientStrength: { value: 1, min: 1, max: 3, step: 0.001 },
-	// 	color: '#e6ff00',
-	// 	speed: { min: 0, max: 20, step: 0.001, value: 4 },
-	// 	noiseStrength: { value: 4, min: 0, max: 10, step: 0.001 },
-	// 	displacementStrength: { min: 0, max: 10, step: 0.01, value: 1.2 },
-	// 	fractAmount: { min: 0, max: 10, step: 0.1, value: 1.2 },
-	// 	roughness: { min: 0, max: 1, step: 0.001, value: 0.1 },
-	// 	metalness: { min: 0, max: 1, step: 0.001, value: 0.1 },
-	// 	clearcoat: { min: 0, max: 1, step: 0.001, value: 1.0 },
-	// 	reflectivity: { min: 0, max: 1, step: 0.001, value: 0.46 },
-	// 	ior: { min: 0.001, max: 5, step: 0.001, value: 0.0 },
-	// 	iridescence: { min: 0, max: 1, step: 0.001, value: 0 },
-	// 	iridescenceIOR: { min: 1, max: 2.333, step: 0.001, value: 1.3 },
-	// 	transmission: { min: 0, max: 1, step: 0.001, value: 0 },
-	// 	thickness: { min: 0, max: 10, step: 0.1, value: 5 },
-	// 	dispersion: { min: 0, max: 20, step: 1, value: 5 },
-	// });
-
-	// const uniformsRef = useRef({
-	// 	uTime: { value: 0 },
-	// 	uColor: { value: new Color(color) },
-	// 	uGradientStrength: { value: gradientStrength },
-	// 	uSpeed: { value: speed },
-	// 	uNoiseStrength: { value: noiseStrength },
-	// 	uDisplacementStrength: { value: displacementStrength },
-	// 	uFractAmount: { value: fractAmount },
-	// });
-
-	// const uniformsRef = {
-	// 	uTime: { value: 0 },
-	// 	uColor: { value: new Color(color) },
-	// 	uGradientStrength: { value: gradientStrength },
-	// 	uSpeed: { value: speed },
-	// 	uNoiseStrength: { value: noiseStrength },
-	// 	uDisplacementStrength: { value: displacementStrength },
-	// 	uFractAmount: { value: fractAmount },
-	// };
-
 	const uniformsRef = useRef({
 		uTime: { value: 0 },
 		uColor: { value: new Color(0xe6ff00) },
-		uGradientStrength: { value: 1 },
 		uSpeed: { value: 4 },
 		uNoiseStrength: { value: 2.5 },
 		uDisplacementStrength: { value: 1 },
@@ -163,18 +100,17 @@ export default memo(function Model() {
 		uIsNormalColor: { value: 0 },
 	});
 
-	useFrame(({ clock, scene, gl, raycaster }, delta) => {
-		const elapsedTime = clock.getElapsedTime();
-
+	useFrame(({ clock, scene, gl, raycaster, invalidate }, delta) => {
 		const inViewEl = [...useDomStore.getState().anchorEls].findLast(el => ScrollTrigger.isInViewport(el, 0.3));
 
-		if (!inViewEl) {
+		if (!inViewEl && ballCloneRef.current) {
 			const epsilon = ballRef.current.position.distanceTo(ballCenterPosRef.current) > 0.005;
 			if (epsilon) {
 				ballRef.current.position.lerp(ballCenterPosRef.current, 0.035);
 				ballMaskRef.current.position.copy(ballRef.current.position);
 				ballCloneRef.current?.position.copy(ballRef.current.position);
 				ballClonedMaskRef.current?.position.copy(ballRef.current.position);
+				ballCloneRef.current.visible = false;
 			}
 		}
 
@@ -183,14 +119,27 @@ export default memo(function Model() {
 		const isMobile = usePlatformStore.getState().isMobile;
 
 		ballMaskRef.current?.scale.set(...(isMobile ? [0.72, 0.72, 0.72] : [1.2, 1.2, 1.2]));
-		ballClonedMaskRef.current?.scale.set(...(isMobile ? [0.72, 0.72, 0.72] : [1.2, 1.2, 1.2]));
+		ballClonedMaskRef.current?.scale.copy(ballMaskRef.current?.scale);
 
 		ballMaterialUpdate(delta);
+		ballRotationUpdate(clock.elapsedTime);
 	});
 
+	function ballRotationUpdate(elapsedTime) {
+		const isBallPress = useWebGlStore.getState().isBallPress;
+		const speed = isBallPress ? 5 : 1.2;
+		const targetRotationX = Math.cos(elapsedTime) * speed;
+		const targetRotationY = Math.sin(elapsedTime) * speed;
+		const targetRotationZ = Math.sin(elapsedTime) * speed;
+
+		ballRef.current.rotation.x = lerp(ballRef.current.rotation.x, targetRotationX, 0.1);
+		ballRef.current.rotation.y = lerp(ballRef.current.rotation.y, targetRotationY, 0.1);
+		ballRef.current.rotation.z = lerp(ballRef.current.rotation.z, targetRotationZ, 0.1);
+	}
+
 	useLenis(event => {
-		updatePosByScroll();
 		scrollOffsetRef.current = event.scroll;
+		updatePosByScroll();
 	});
 
 	function updatePosByScroll() {
@@ -224,7 +173,7 @@ export default memo(function Model() {
 			const { x: mirrorX, y: mirrorY } = getElementPosition(inViewMirrorEl);
 			const targetBallClonePos = ballClonedDynamicPosRef.current.set(mirrorX, mirrorY, 1);
 			ballCloneRef.current.position.lerp(targetBallClonePos, 0.035);
-			ballClonedMaskRef.current.position.copy(targetBallClonePos);
+			ballClonedMaskRef.current.position.copy(ballCloneRef.current.position);
 			ballCloneRef.current.visible = true;
 		} else {
 			ballCloneRef.current.position.lerp(targetBallPos, 0.035);
@@ -235,7 +184,7 @@ export default memo(function Model() {
 	useEffect(() => {
 		const { scene } = getThree();
 		ballCloneRef.current = ballRef.current.clone();
-		ballCloneRef.current.name = 'psychedelic-ball-cloned';
+		ballCloneRef.current.name = 'psychedelic-ball-cloned-mesh';
 		ballCloneRef.current.visible = false;
 		scene.add(ballCloneRef.current);
 	}, []);
@@ -270,6 +219,8 @@ export default memo(function Model() {
 			displacementScale: 0,
 			silent: true,
 			transparent: true,
+			side: FrontSide,
+			blending: NoBlending,
 		}),
 	);
 
@@ -278,9 +229,10 @@ export default memo(function Model() {
 	return (
 		<group>
 			<mesh
-				name='psychedelic-ball'
+				name='psychedelic-ball-mesh'
+				raycast={meshBounds}
 				ref={ballRef}
-				geometry={ballGeometry}
+				geometry={ballGeometry.current}
 				position={ballInitPosRef.current}
 				material={ballMaterialRef.current}
 				frustumCulled={false}></mesh>
@@ -295,68 +247,61 @@ export default memo(function Model() {
 });
 
 function MaskBall({ ballRef, ballMaskRef, ballClonedMaskRef }) {
-	const [isBallPress, setIsBallPress] = useState(false);
+	const isBallPress = useWebGlStore(state => state.isBallPress);
 	const ballMaskGeo = useRef(new CircleGeometry(1, 8));
 	const ballMaskMaterialRef = useRef(
-		new MeshBasicMaterial({ visible: true, depthTest: false, depthWrite: false, transparent: true, opacity: 0 }),
+		new MeshBasicMaterial({
+			visible: true,
+			depthTest: false,
+			depthWrite: false,
+			transparent: true,
+			opacity: 0,
+			side: FrontSide,
+		}),
 	);
-
-	useFrame(({ clock, scene, gl, raycaster }) => {
-		const elapsedTime = clock.getElapsedTime();
-		ballRotationRefUpdate(elapsedTime);
-	});
-
-	function ballRotationRefUpdate(elapsedTime) {
-		const speed = isBallPress ? 4 : 1.2;
-		const targetRotationX = Math.cos(elapsedTime) * speed;
-		const targetRotationY = Math.sin(elapsedTime) * speed;
-		const targetRotationZ = Math.sin(elapsedTime) * speed;
-
-		ballRef.current.rotation.x = lerp(ballRef.current.rotation.x, targetRotationX, 0.1);
-		ballRef.current.rotation.y = lerp(ballRef.current.rotation.y, targetRotationY, 0.1);
-		ballRef.current.rotation.z = lerp(ballRef.current.rotation.z, targetRotationZ, 0.1);
-	}
 
 	useGSAP(
 		() => {
 			if (ballRef.current && ballRef.current.material) {
 				if (isBallPress) {
-					gsap.to(ballRef.current.material.uniforms.uNoiseStrength, {
-						value: 3.5,
-						duration: 3,
-						ease: 'elastic.out(1, 0.1)',
-					});
 					gsap.to(ballRef.current.material.uniforms.uDisplacementStrength, {
-						value: 1.1,
-						duration: 3,
-						ease: 'elastic.out(1, 0.1)',
+						value: 1.5,
+						duration: 0.5,
+						ease: 'bounce.out',
+					});
+					gsap.to(ballRef.current.material.uniforms.uNoiseStrength, {
+						value: 1,
+						duration: 2,
+						ease: 'bounce.out',
 					});
 					gsap.to(ballRef.current.material, {
 						iridescence: 1,
 						metalness: 0.6,
+						ior: 0.4,
 						roughness: 1,
 						clearcoat: 0,
-						duration: 3,
-						ease: 'elastic.out(1, 0.1)',
+						duration: 2,
+						ease: 'bounce.out',
 					});
 				} else {
-					gsap.to(ballRef.current.material.uniforms.uNoiseStrength, {
-						value: 2.5,
-						duration: 3,
-						ease: 'elastic.out(1, 0.1)',
-					});
 					gsap.to(ballRef.current.material.uniforms.uDisplacementStrength, {
 						value: 1,
-						duration: 3,
-						ease: 'elastic.out(1, 0.1)',
+						duration: 0.5,
+						ease: 'bounce.in',
+					});
+					gsap.to(ballRef.current.material.uniforms.uNoiseStrength, {
+						value: 2.5,
+						duration: 2,
+						ease: 'bounce.in',
 					});
 					gsap.to(ballRef.current.material, {
 						iridescence: 0.1,
+						ior: 0,
 						metalness: 0.5,
 						roughness: 0.1,
 						clearcoat: 1.0,
-						duration: 3,
-						ease: 'elastic.out(1, 0.1)',
+						duration: 2,
+						ease: 'bounce.in',
 					});
 				}
 			}
@@ -371,11 +316,21 @@ function MaskBall({ ballRef, ballMaskRef, ballClonedMaskRef }) {
 				ref={ballMaskRef}
 				position={[0, 0, -1]}
 				material={ballMaskMaterialRef.current}
-				onPointerDown={e => setIsBallPress(true)}
-				onPointerUp={e => setIsBallPress(false)}
-				onPointerOver={e => (document.body.style.cursor = 'pointer')}
+				onPointerDown={e => {
+					e.stopPropagation();
+					useWebGlStore.setState({ isBallPress: true });
+				}}
+				onPointerUp={e => {
+					e.stopPropagation();
+					useWebGlStore.setState({ isBallPress: false });
+				}}
+				onPointerOver={e => {
+					e.stopPropagation();
+					document.body.style.cursor = 'pointer';
+				}}
 				onPointerOut={e => {
-					setIsBallPress(false);
+					e.stopPropagation();
+					useWebGlStore.setState({ isBallPress: false });
 					document.body.style.cursor = 'default';
 				}}
 				scale={1.2}
@@ -384,12 +339,22 @@ function MaskBall({ ballRef, ballMaskRef, ballClonedMaskRef }) {
 				raycast={meshBounds}
 				ref={ballClonedMaskRef}
 				material={ballMaskMaterialRef.current}
-				position={[0, 0, 1]}
-				onPointerDown={e => setIsBallPress(true)}
-				onPointerUp={e => setIsBallPress(false)}
-				onPointerOver={e => (document.body.style.cursor = 'pointer')}
+				position={[0, 0, -1]}
+				onPointerDown={e => {
+					e.stopPropagation();
+					useWebGlStore.setState({ isBallPress: true });
+				}}
+				onPointerUp={e => {
+					e.stopPropagation();
+					useWebGlStore.setState({ isBallPress: false });
+				}}
+				onPointerOver={e => {
+					e.stopPropagation();
+					document.body.style.cursor = 'pointer';
+				}}
 				onPointerOut={e => {
-					setIsBallPress(false);
+					e.stopPropagation();
+					useWebGlStore.setState({ isBallPress: false });
 					document.body.style.cursor = 'default';
 				}}
 				scale={1.2}
