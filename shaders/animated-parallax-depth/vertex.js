9cdbcf609cbada8b9ebb367c6a17f9d78a9c554c
laynechensquare <laynechensquare@gmail.com>
2024-10-30 22:12:22 +0800

    refactor: shader renaming for clarity

--- shaders/animated-parallax-depth/vertex.js
+++ shaders/animated-parallax-depth/vertex.js
@@ -6,13 +6,13 @@ varying vec2 vUv;
 varying vec3 vPosition;
 
 
-uniform float uIsInView;
+uniform float uShouldSample;
 varying float vIsInView;
 
 void main() {
     vUv = uv;
     vPosition = position;
-    vIsInView = uIsInView;
+    vIsInView = uShouldSample;
 
     // csm_PositionRaw = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
