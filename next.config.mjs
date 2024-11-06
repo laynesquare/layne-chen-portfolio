cd286462ea45a8dd826dc80ddd20c17cb94cec47
laynechensquare <laynechensquare@gmail.com>
2024-11-06 18:33:06 +0800

    feat: ssg config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
 	webpack: (config, options) => {
 		config.module.rules.push({
 			test: /\.(glsl|vs|fs|vert|frag)$/,
